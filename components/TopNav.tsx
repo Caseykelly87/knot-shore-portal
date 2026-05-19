0832501a3987d9063927ca8948c8b77bfc1a074c
Casey Kelly <Caseykelly87@gmail.com>
2026-05-19 11:46:05 -0500

    feat(nav): add stores link to top navigation

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 
 const NAV_ITEMS = [
   { href: "/", label: "Dashboard" },
+  { href: "/stores", label: "Stores" },
   { href: "/exceptions", label: "Exceptions" },
   { href: "/about", label: "About" },
 ];
